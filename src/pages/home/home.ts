420280c6652d35985f7226237b98b2ed30bd61c4
JdGXII <jralzaibar@yahoo.com>
2018-06-30 15:08:57 -0400

    added login page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,10 +7,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   menuData = [
-    {title: 'Menu Principal', pushPage: 'MenuPage'},
-    {title: 'Cuenta', pushPage: 'AccountPage'},
-    {title: 'Acerca de nosotros', pushPage: 'AboutPage'},
-    {title: 'Tipos de trabajos', pushPage: 'WorksPage'}
+    {title: 'Menu Principal', pic: '../assets/imgs/uha-all-hands-in-1.jpg', pushPage: 'MenuPage'},
+    {title: 'Cuenta', pic: '../assets/imgs/uha-all-hands-in-1.jpg', pushPage: 'AccountPage'},
+    {title: 'Acerca de nosotros', pic: '../assets/imgs/uha-all-hands-in-1.jpg', pushPage: 'AboutPage'},
+    {title: 'Tipos de trabajos', pic: '../assets/imgs/uha-all-hands-in-1.jpg', pushPage: 'WorksPage'}
   ];
   constructor(public navCtrl: NavController) {
 
