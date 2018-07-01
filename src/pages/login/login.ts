a222229a923dd98ce339927e6bc632c88a2e42a9
JdGXII <jralzaibar@yahoo.com>
2018-06-30 22:18:59 -0400

    user login implemented. User login after registering implemented with user service

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from '../home/home';
+import {UserServiceProvider} from '../../providers/user-service/user-service';
 
 /**
  * Generated class for the LoginPage page.
@@ -17,12 +19,36 @@ export class LoginPage {
 
   regPage: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  login = {
+    email: '',
+    password: ''
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private userService: UserServiceProvider) {
     this.regPage = 'RegisterPage';  
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  signOn(){
+    if(!this.login.email || !this.login.password){
+      this.userService.displayAlert("Error", "Debes ingresar correo y contraseña");
+    }
+    else{
+      this.userService.logOn(this.login.email, this.login.password)
+      .then(returned => {
+        if(this.userService.success){
+          this.navCtrl.push(HomePage);
+        }
+        else{
+          this.login.email = '';
+          this.login.password = '';
+        }
+      })
+    }
   }
 
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { AngularFireAuth } from 'angularfire2/auth';
+import {UserServiceProvider} from '../../providers/user-service/user-service';
 /**
  * Generated class for the RegisterPage page.
  *
@@ -23,7 +24,8 @@ export class RegisterPage {
   };
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public alertCtrl: AlertController, private afAuth: AngularFireAuth) {
+              public alertCtrl: AlertController, private afAuth: AngularFireAuth,
+              private userService: UserServiceProvider) {
   }
 
   ionViewDidLoad() {
@@ -61,8 +63,7 @@ export class RegisterPage {
 
   regSuccess(result){
     this.displayAlert(result.email, 'Cuenta creada con exito');
-    this.afAuth.auth.signInWithEmailAndPassword(this.reg.email, this.reg.password1)
-    .then(res => this.navCtrl.push(HomePage))
-    .catch(err => this.displayAlert('Error', err))
+    this.userService.logOn(this.reg.email, this.reg.password1)
+    .then(res => this.navCtrl.push(HomePage))    
   }
 }
